4f0caf442ba1a39b3e902422ed4e66fb9ab32cfe
Yunus Emre Durmuş <ysemredurmus@gmail.com>
2024-03-19 14:45:12 +0300

    Update project configuration Firebase  and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:joysper_homework1/feature/home/screens/home_screen.dart';
 import 'package:joysper_homework1/feature/home/screens/register_screen.dart';
 import 'package:joysper_homework1/feature/splash/screens/splash_screen.dart';
+import 'package:joysper_homework1/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
